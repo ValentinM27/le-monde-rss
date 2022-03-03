0d03d31fb023afaaad246e0ebaff0302184bc048
Valentin Marguerie <74249732+ValentinM27@users.noreply.github.com>
2022-03-03 08:18:42 +0100

    Setup for Netlify

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ export default class App extends React.Component{
       <Router>
         <Navigation />
         <Routes>
-          <Route path="/economie" element={<Parser link={"https://www.lemonde.fr/economie/rss_full.xml"}/>}/>
-          <Route path="/" element={<Parser link={'https://www.lemonde.fr/rss/une.xml'}/>}/>
-          <Route path="/france" element={<Parser link={'https://www.lemonde.fr/societe/rss_full.xml'}/>}/>
-          <Route path="/monde" element={<Parser link={'https://www.lemonde.fr/international/rss_full.xml'}/>}/>
-          <Route path="/politique" element={<Parser link={'https://www.lemonde.fr/politique/rss_full.xml'}/>}/>
-          <Route path="/sciences" element={<Parser link={'https://www.lemonde.fr/sciences/rss_full.xml'}/>}/>
+          <Route exact path="/economie" element={<Parser link={"https://www.lemonde.fr/economie/rss_full.xml"}/>}/>
+          <Route exact path="/" element={<Parser link={'https://www.lemonde.fr/rss/une.xml'}/>}/>
+          <Route exact path="/france" element={<Parser link={'https://www.lemonde.fr/societe/rss_full.xml'}/>}/>
+          <Route exact path="/monde" element={<Parser link={'https://www.lemonde.fr/international/rss_full.xml'}/>}/>
+          <Route exact path="/politique" element={<Parser link={'https://www.lemonde.fr/politique/rss_full.xml'}/>}/>
+          <Route exact path="/sciences" element={<Parser link={'https://www.lemonde.fr/sciences/rss_full.xml'}/>}/>
           <Route path='*' element={<NotFound />} />
         </Routes>
       </Router>
